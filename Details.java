d6447c8f87ee3b2897fa91b2e3ca683de10b1a1c
Renukeshdurgaprasad <153423672+Renukeshdurgaprasad@users.noreply.github.com>
2025-03-14 11:21:29 +0530

    Create Details.java

---
+++ Details.java
@@ -0,0 +1,58 @@
+package cusDetails;
+public class Details{
+ public final  String n[]=new String[12];
+ public final  String id[]=new String[12];
+ public  long p[]=new long[12];
+ public  long o[]={1,1,1,1,1,1,1,1,1,1,1,1};
+ public  double amo[]=new double[12];
+    public Details(){
+       n[0]="Nikhil";
+       id[0]="901234772389";
+       p[0]=2007;
+       amo[0]=15000;
+       n[1]="Renukesh";
+       id[1]="901234789774";
+       p[1]=7981;
+       amo[1]=20000;
+       n[2]="durgaprasad";
+       id[2]="801982345341";
+       p[2]=1787;
+       amo[2]=25000;
+       n[3]="Ganesh";
+       id[3]="901547754399";
+       p[3]=3454;
+       amo[3]=50000;
+       n[4]="Krishna";
+       id[4]="897877777399";
+       p[4]=2323;
+       amo[4]=20000;   
+       n[5]="rafeed";
+       id[5]="778877897799";
+       p[5]=4455;
+       amo[5]=24300;
+       n[6]="Surya";
+       id[6]="844552127399";
+       p[6]=2575;
+       amo[6]=24000;
+       n[7]="dinesh";
+       id[7]="844528272723";
+       p[7]=9089;
+       amo[7]=18000;
+       n[8]="Bhanu";
+       id[8]="678745127399";
+       p[8]=6657;
+       amo[8]=30000; 
+       n[9]="Vineeth";
+       id[9]="899099834399";
+       p[9]=1236;
+       amo[9]=27800;
+       n[10]="Vishwanad";
+       id[10]="798502786399";
+       p[10]=9976;
+       amo[10]=26800;
+       n[11]="Manikanta";
+       id[11]="897454431399";
+       p[11]=9087;
+       amo[11]=45000;
+}
+}
